2f5964c97102672b92a6f436855dea216a8ba8d2
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-06-28 14:25:28 +0700

    dang nhap da ngon ngu

---
+++ src/main/java/hoang/codegym/controller/UserController.java
@@ -0,0 +1,18 @@
+package hoang.codegym.controller;
+
+import hoang.codegym.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/login")
+    public ModelAndView formLogin(){
+        ModelAndView mav = new ModelAndView("/login");
+        mav.addObject("user", new User());
+        return mav;
+
+    }
+}
